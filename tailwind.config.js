a8c0ff2c07943417ae91840d89ea0ecad23ac436
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2024-08-04 09:59:18 +0200

    fixed an issue were tailwind wasn't working properly on other than .js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./**/index.html', './src/**/*.{js, ts, jsx, tsx, mjs, cjs}'],
+  content: ['./**/index.html', './src/**/*.{js,ts,jsx,tsx,mjs,cjs}'],
   theme: {
     extend: {},
   },
